e83667b85013582d0b990ff865d8e687dc0d8f95
mukulsarswat <137614804+mukulsarswat@users.noreply.github.com>
2025-08-20 22:54:31 +0530

    Create mux_4_X_1.v
    Using 2:1 MUX

---
+++ 7.MUX/7.2.MUX_4_X_1/mux_4_X_1.v
@@ -0,0 +1,21 @@
+// IMPLEMENT 4:1 using 2:1 MUX. 
+module two_x_one(d0,d1,sel,y);
+	input d0,d1,sel;
+	output y;
+
+	assign y=(d0&(~sel))|(d1&sel);
+endmodule
+
+module four_x_one(d0, d1, d2, d3, sel, out);
+  input d0, d1, d2, d3;
+  input [1:0] sel;
+  output out;
+  wire w1, w2;
+
+  // First level: select between d0/d1 and d2/d3
+  two_x_one t1(d0, d1, sel[0], w1);
+  two_x_one t2(d2, d3, sel[0], w2);
+
+  // Second level: select between w1 and w2
+  two_x_one t3(w1, w2, sel[1], out);
+endmodule
