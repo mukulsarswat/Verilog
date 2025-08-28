1a97864f7697bf47ffb459b08dc41c801fa1684c
MUKUL SARSWAT <137614804+mukulsarswat@users.noreply.github.com>
2025-08-28 10:05:35 +0530

    Create 7_seg_dis_tb.v

---
+++ 13.7_Segment_Display/7_seg_dis_tb.v
@@ -0,0 +1,49 @@
+
+module tb();
+	reg [3:0] hex;
+	wire [6:0] seg;
+	hex_to_7_segment uut (hex,seg);
+  initial
+		begin
+          $dumpfile("wave.vcd");
+          $dumpvars(0, uut);
+
+         end
+                    
+	initial
+		begin
+          hex = 4'h0;
+          #10;
+          hex = 4'h1;
+          #10;
+          hex = 4'h2;
+          #10;
+          hex = 4'h3;
+          #10;
+          hex = 4'h4;
+          #10;
+          hex = 4'h5;
+          #10;
+          hex = 4'h6;
+          #10;
+          hex = 4'h7;
+          #10;
+          hex = 4'h8;
+          #10;
+          hex = 4'h9;
+          #10;
+          hex = 4'hA;
+          #10;
+          hex = 4'hB;
+          #10;
+          hex = 4'hC;
+          #10;
+          hex = 4'hD;
+          #10;
+          hex = 4'hE;
+          #10;
+          hex = 4'hF;
+          #10;
+          $finish;
+		end
+endmodule
