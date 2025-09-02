a4e3bc541dd05b26e771ef184528ad67c1a963f3
MUKUL SARSWAT <137614804+mukulsarswat@users.noreply.github.com>
2025-09-03 02:50:11 +0530

    Create 1101_seq_tb.v

---
+++ 17.Seq_Detector_1101/1101_seq_tb.v
@@ -0,0 +1,39 @@
+// Code your testbench here
+// or browse Examples
+`timescale 1ns / 1ps
+module tb_sequence_detector_1101;
+
+reg clk, rst, in_bit;
+wire detected;
+
+sequence_detector_1101 uut (
+    .clk(clk),
+    .rst(rst),
+    .in_bit(in_bit),
+    .detected(detected)
+);
+
+initial begin
+    $dumpfile("sequence_detector_1101.vcd");
+    $dumpvars(0, tb_sequence_detector_1101);
+end
+
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+initial begin
+    rst = 1; in_bit = 0; #10;
+    rst = 0;
+
+    in_bit = 1; #10;
+    in_bit = 1; #10;
+    in_bit = 0; #10;
+    in_bit = 1; #10;
+
+    $finish;
+end
+
+
+endmodule
